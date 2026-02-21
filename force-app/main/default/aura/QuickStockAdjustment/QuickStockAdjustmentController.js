eaecfd6ca2ec7636054990158a85c838ded22cbf
Aphiwe Zulu <azulu058@gmail.com>
2026-02-21 18:42:43 +0200

    Changed the attribute name of value to adjust by from "valueToIncrement" to "valueToAdjustBy". Added logic to increment and decrement the value of the valueToAdjustBy attribute to the aura JS controller.

--- force-app/main/default/aura/QuickStockAdjustment/QuickStockAdjustmentController.js
+++ force-app/main/default/aura/QuickStockAdjustment/QuickStockAdjustmentController.js
@@ -1,10 +1,14 @@
 ({
     handleIncrement : function(component, event, helper) {
+        const currentValue = component.get('v.valueToAdjustBy')
+        component.set('v.valueToAdjustBy', currentValue + 1)
         const productID = component.get('v.searchedProduct').Id
         helper.increaseStock(component, productID)
     },
 
      handleDecrement : function(component, event, helper) {
+        const currentValue = component.get('v.valueToAdjustBy')
+        component.set('v.valueToAdjustBy', currentValue - 1)
         const productID = component.get('v.searchedProduct').Id
         helper.decreaseStock(component, productID)
     },
